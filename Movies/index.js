d4297a6e73e8ee19c65b23a32b14bd6c87d9bca5
MoNNsterFTW <iozsaandrei55@gmail.com>
2020-07-25 08:56:41 +0300

    debounce

--- Movies/index.js
+++ Movies/index.js
@@ -1,1 +1,21 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'eb3d28d1',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+
+
+
+const onInput = event => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 800));
